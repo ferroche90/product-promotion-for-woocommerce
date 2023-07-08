1352a7bc271e7f486a56da0c6e02540fb13961e4
Fer Roche <fergrochev@gmail.com>
2023-07-08 19:36:33 -0400

    datetime bug fixed

--- js/admin.js
+++ js/admin.js
@@ -1,21 +1,23 @@
 jQuery(document).ready(function($) {
-    // Show datetime picker on checkbox checked
+    // Show date picker on checkbox checked
     $('input#_set_expiration').change(function() {
-        if ($(this).is(":checked")) {
-            $('p._expiration_datetime_field').show();
+        if($(this).is(":checked")) {
+            $('p._expiration_date_field').show();
         } else {
-            $('p._expiration_datetime_field').hide();
+            $('p._expiration_date_field').hide();
         }
     }).change();
 
     // Make adding an expiration date mandatory if checkbox is checked
     $('form#post').on('submit', function(e) {
         var setExpirationChecked = $('#_set_expiration').is(':checked');
-        var expirationDatetimeEmpty = !$('#_expiration_datetime').val();
+        var expirationDateEmpty = !$('#_expiration_date').val();
 
-        if (setExpirationChecked && expirationDatetimeEmpty) {
-            alert('Please enter an expiration date and time for the promotion.');
+        if (setExpirationChecked && expirationDateEmpty) {
+            alert('Please enter an expiration date for the promotion.');
             e.preventDefault();
         }
     });
 });
+
+
